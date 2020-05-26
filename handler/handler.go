58b68a8f664d9ce88a97a7a22f1cd758aa5fa85c
Dmitry Momot <mail@dmomot.com>
2020-05-26 19:46:46 +0300

    wip

--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,17 @@
 package handler
 
 import (
+	"database/sql"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"net/url"
 
+	"github.com/dmitrymomot/media-srv/repository"
+	"github.com/dmitrymomot/media-srv/storage"
 	"github.com/go-chi/render"
+	"github.com/pkg/errors"
 )
 
 type (
@@ -19,6 +25,24 @@ type (
 	HTTPError struct {
 		Code    int
 		Message interface{}
+	}
+
+	resizerFunc func(f io.ReadCloser, w, h int) (io.ReadSeeker, error)
+
+	logger interface {
+		Debug(v ...interface{})
+		Info(v ...interface{})
+		Warn(v ...interface{})
+		Error(v ...interface{})
+	}
+
+	// Handler struct
+	Handler struct {
+		db      *sql.DB
+		query   *repository.Queries
+		storage *storage.Interactor
+		resize  resizerFunc
+		log     logger
 	}
 )
 
@@ -44,8 +68,21 @@ func (h Wrap) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			render.Status(r, er.Code)
 			render.JSON(w, r, render.M{"error": er.Message})
 		default:
+			log.Println(errors.Wrap(err, "undefined http error"))
 			render.Status(r, http.StatusInternalServerError)
 			render.JSON(w, r, render.M{"error": err.Error()})
 		}
+	}
+}
+
+// New is a factory function,
+// returns a new instance of the HTTP handler interactor
+func New(db *sql.DB, q *repository.Queries, s *storage.Interactor, rf resizerFunc, l logger) *Handler {
+	return &Handler{
+		db:      db,
+		query:   q,
+		storage: s,
+		resize:  rf,
+		log:     l,
 	}
 }
